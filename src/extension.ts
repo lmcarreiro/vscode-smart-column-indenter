c3e78476d7288d6220db0ccf768742f3fbe18eed
Leonardo Carreiro <leonardo.mcarreiro@gmail.com>
2017-11-12 13:10:24 -0200

    Create commands, keybindings and menu items

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,8 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+    const disposables: vscode.Disposable[] = [];
+
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "smart-column-indenter" is now active!');
@@ -14,14 +16,32 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+
+    disposables.push(vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
-    });
+    }));
 
-    context.subscriptions.push(disposable);
+    disposables.push(vscode.commands.registerTextEditorCommand("extension.smartColumnIndenter.indent2", indent2));
+    disposables.push(vscode.commands.registerTextEditorCommand("extension.smartColumnIndenter.indentN", indentN));
+    disposables.push(vscode.commands.registerTextEditorCommand("extension.smartColumnIndenter.removeLineBreaks", removeLineBreaks));
+
+    context.subscriptions.push(...disposables);
+}
+
+function indent2(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]): void
+{
+    vscode.window.showInformationMessage('indent2');
+}
+function indentN(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]): void
+{
+    vscode.window.showInformationMessage('indentN');
+}
+function removeLineBreaks(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]): void
+{
+    vscode.window.showInformationMessage('removeLineBreaks');
 }
 
 // this method is called when your extension is deactivated
